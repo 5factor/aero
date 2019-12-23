194d0f34cacdbe654b874d313290d87b4d278b12
UndecidedFactor <53883918+UndecidedSource@users.noreply.github.com>
2019-12-22 20:07:05 -0500

    Create setModeration.js

---
+++ src/commands/setModeration.js
@@ -0,0 +1,41 @@
+const { RichEmbed } = require('discord.js');
+const { Colors } = require('../config');
+const database = require('../util/database');
+
+module.exports.run = async (client, message, args, { guild, error }) => {
+	try {
+		const allowed = ["kick", "ban", "softban"]
+
+		const pointAmount = parseInt(args[0]);
+		const modAction = args[1];
+
+		if (!pointAmount || !allowed.includes(modAction.toLowerCase())) return message.channel.send(
+			new RichEmbed().setTitle('Automated Moderation').setColor(Colors.FAILED).setDescription('Incorrect usage. You must include a valid amount of points and a valid action (kick, ban, softban).').setFooter(message.author.tag, message.author.displayAvatarURL)
+		);
+
+		const newArr = guild.moderation;
+		newArr[modAction] = pointAmount;
+		database.guilds.update(message.guild.id, { moderation: newArr });
+
+		const filterEmbed = new RichEmbed()
+			.setTitle('Automated Moderation')
+			.setColor(Colors.SUCCESS)
+			.setDescription(`**Moderator**: ${message.author.tag}\n**Points** ${pointAmount}\n**Action**: ${modAction}`)
+			.setFooter(message.author.tag, message.author.displayAvatarURL)
+			.setTimestamp();
+
+		message.channel.send(filterEmbed);
+
+	}
+	catch (e) {
+		error(e.stack);
+	}
+};
+
+module.exports.data = {
+	name: 'setmoderation',
+	description: 'Set an automated moderation action for when a user hits a certain amount of points',
+	type: 'mod',
+	usage: ['!setmoderation <pointsAmount> <kick/ban/softban>'],
+	aliases: null,
+};
