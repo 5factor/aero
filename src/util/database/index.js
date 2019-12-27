07586fa8a37f71e5d9ab969ba39a9d8c60ff6aea
UndecidedSource <53883918+UndecidedSource@users.noreply.github.com>
2019-12-26 23:35:43 -0500

    Update index.js

--- src/util/database/index.js
+++ src/util/database/index.js
@@ -21,11 +21,6 @@ database.guilds.create = async (id, {
 	wordblacklist = [],
 	commandchannels = [],
 } = {}) => {
-	const data = { premium, prefix, linkFilter, modrole, adminrole, wordblacklist, commandchannels, moderation, wordblacklistactive, leveling, economy, economyactive, levelingactive };
-	const existing = await database.collection("guilds").get(id);
-	if (existing) return existing;
-	await database.collection("guilds").insert(id, data);
-	return { id, ...data };
 };
 
 database.guilds.fetch = async (id) => {
