2b90a8e067164b30b2d8c90a7702ab82b7b06598
UndecidedSource <53883918+UndecidedSource@users.noreply.github.com>
2019-12-29 14:48:26 -0500

    Attempt to fix automated moderation

--- src/util/functions/userWarn.js
+++ src/util/functions/userWarn.js
@@ -1,11 +1,11 @@
 const nil = require("./nil");
 
-module.exports = async (message, user, guild) => {
+module.exports = async (message, userId, guild) => {
     const { author: userObj, guild: guildObj } = message;
     const moderation = new Map(guild.moderation) || [];
     const warnings = new Map(guild.warnings) || [];
 
-    const warnArr = warnings.get(user._id) || [];
+    const warnArr = warnings.get(userId) || [];
     if (!warnArr.length) return;
 
     const points = warnArr.reduce((acc, cur) => acc + cur.points, 0);
@@ -14,14 +14,14 @@ module.exports = async (message, user, guild) => {
     if (points >= moderation.kick) {
         await userObj.send(`You have been kicked from ${guildObj.name} for recieving ${moderation.kick} warnings!`)
             .catch(nil);
-        await guildObj.kick(user._id, `Recieved ${moderation.kick} warnings.`)
+        await guildObj.kick(userId, `Recieved ${moderation.kick} warnings.`)
             .catch(nil);
     }
 
     if (points >= moderation.ban) {
         await userObj.send(`You have been banned from ${guildObj.name} for recieving ${moderation.ban} warnings!`)
             .catch(nil);
-        await guildObj.ban(user._id, `Recieved ${moderation.ban} warnings.`)
+        await guildObj.ban(userId, `Recieved ${moderation.ban} warnings.`)
             .catch(nil);
     }
 };
