4a8a8a0610599c25a1ca5d63034575a4885e66b2
ProfessionalRBX <43869637+ProfessionalRBX@users.noreply.github.com>
2020-02-03 22:39:00 -0500

    Update userWarn.js

--- src/util/functions/userWarn.js
+++ src/util/functions/userWarn.js
@@ -8,11 +8,8 @@ module.exports = async (client, member, userId, guild, guildName) => {
     if (!warnArr.length) return;
 
     const points = warnArr.reduce((acc, cur) => acc + cur.points, 0);
-    console.log(points);
-    console.log(moderation)
 
     if (points >= moderation.get("kick")) {
-        console.log("just testing this");
         await client.users.get(userId).send(`You have been kicked from ${guildName} for recieving ${moderation.kick} warnings!`)
             .catch(e => console.log(e));
         await member.kick(`Recieved ${moderation.kick} warnings.`)
@@ -20,7 +17,6 @@ module.exports = async (client, member, userId, guild, guildName) => {
     }
 
     if (points >= moderation.get("ban")) {
-        console.log("just testing this also");
         await client.users.get(userId).send(`You have been banned from ${guildName} for recieving ${moderation.ban} warnings!`)
             .catch(e => console.log(e));
         await member.ban(userId, `Recieved ${moderation.ban} warnings.`)
