9856e57d6ce09a4a6283f1481d417c2a5543be96
UndecidedFactor <53883918+UndecidedSource@users.noreply.github.com>
2020-02-26 18:51:55 -0500

    Update ready.js

--- src/events/ready.js
+++ src/events/ready.js
@@ -4,19 +4,10 @@ const { Info, Logging } = require('../config');
 module.exports = async (client) => {
 
 	const username = Info.NAME;
-	const startChannel = Logging.RESTART_CHANNEL;
 
 	console.log(`Logged in as ${client.user.tag} (${client.user.id})`);
 	client.user.setActivity('Beep boop', {
 		type: 'STREAMING',
 		url: 'https://twitch.tv/undecidedfactor',
 	});
-
-	const readyEmbed = new RichEmbed()
-		.setTitle(`${username}: Process Started`)
-		.setColor('3483eb')
-		.setDescription(`${username} has successfully logged in`)
-		.setTimestamp();
-
-	client.channels.get(startChannel).send(readyEmbed);
 };
